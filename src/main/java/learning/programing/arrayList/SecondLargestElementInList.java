f3a601b7678be3ab250f00466c86c2460db5a784
sunilkumarbs <sunilkumarbs05@gmail.com>
2025-06-17 22:08:31 +0530

    Updated

--- src/main/java/learning/programing/arrayList/SecondLargestElementInList.java
+++ src/main/java/learning/programing/arrayList/SecondLargestElementInList.java
@@ -1,13 +1,14 @@
 package learning.programing.arrayList;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class SecondLargestElementInList {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(10, 20, 30, 5);
+        List<Integer> list = Arrays.asList(10, 20, 30, 5, 1, 40, 30, 3, 8);
         System.out.println("Input: " + list);
-        Integer result = list.stream().sorted((v1, v2) -> v2.compareTo(v1)).limit(2).skip(1).findFirst().get();
+        Integer result = list.stream().sorted(Comparator.reverseOrder()).limit(2).skip(1).findFirst().get();
         System.out.println("Second largest from the array list is : " + result);
     }
 }
